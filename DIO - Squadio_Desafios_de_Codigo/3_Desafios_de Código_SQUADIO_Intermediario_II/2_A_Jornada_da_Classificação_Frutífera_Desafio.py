4c91acd1eeefa103912faded1194a2ec4f6a0355
Erick Dias <140857951+erickdias10@users.noreply.github.com>
2024-04-28 17:50:17 -0300

    Add files via upload

---
+++ DIO - Squadio_Desafios_de_Codigo/3_Desafios_de Código_SQUADIO_Intermediario_II/2_A_Jornada_da_Classificação_Frutífera_Desafio.py
@@ -0,0 +1,38 @@
+#Desafio: A Jornada da Classificação Frutífera
+
+"""
+Descrição
+Nesta missão, sua tarefa é mais desafiadora do que nunca! Em um pomar mágico, as frutas têm características únicas que as diferenciam. Seu objetivo é criar um modelo de machine learning capaz de classificar frutas com base em três características: peso, textura (suave ou áspera) e cor (vermelha, laranja ou amarela). Cada tipo de fruta tem limites específicos para essas características.
+
+Maçã:
+
+Peso mínimo: 130 gramas
+Textura: Ápera (rough)
+Cor: Vermelha (red)
+Laranja:
+
+Peso mínimo: 120 gramas
+Textura: Suave (smooth)
+Cor: Laranja (orange)
+Morango:
+
+Peso mínimo: 150 gramas
+Textura: Suave (smooth)
+Cor: Vermelha (red)
+Banana:
+
+Peso mínimo: 150 gramas
+Textura: Áspera (rough)
+Cor: Amarela (yellow)
+Entrada
+Seu código deve receber as seguintes entradas do usuário:
+
+Peso da fruta (em gramas): um número real que representa o peso da fruta.
+Textura da fruta (suave ou áspera): uma string indicando se a fruta é suave ("smooth") ou áspera ("rough").
+Cor da fruta (vermelha, laranja ou amarela): uma string indicando a cor da fruta.
+Saída
+O código deve produzir uma saída indicando a classificação da fruta com base nas características fornecidas.
+
+Exemplos
+A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+"""
